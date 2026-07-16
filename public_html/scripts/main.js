422b0fd7ad4abb101b90d3be8dad1a9c2e102a08
wolfgang resendiz <wolf.resendiz@outlook.com>
2026-07-16 14:35:46 -0600

    Delete public_html directory

--- public_html/scripts/main.js
+++
@@ -1,9 +0,0 @@
-let miImage = document.querySelector('img');
-miImage.onclick = function () {
-    let miSrc = miImage.getAttribute('src');
-    if (miSrc === 'images/gato.png') {
-      miImage.setAttribute('src','images/cat.png');
-    } else {
-      miImage.setAttribute('src', 'images/gato.png');
-    }
-}
